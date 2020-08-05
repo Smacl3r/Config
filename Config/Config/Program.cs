0a494fa1debf8c2db58718e7eda2d455742da9d3
dalzuk <dalius.zukauskas@ktu.edu>
2020-08-05 16:07:55 +0300

    Add comments and exception handling

--- Config/Config/Program.cs
+++ Config/Config/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Text.RegularExpressions;
 
+
+/*
+ Please do also provide some thoughts (in textual form only) on how the following requirement could be implemented:
+- it should provide information if variability constraints are violated 
+	- Example: The parameter 'powerSupply' is required to be set to 'big' if the number of aisles in the sub-system config is >=5.
+
+ Creating validation helper class for Configuration to check if any of the boundaries were violated.
+ */
 namespace Config
 {
     class Program
@@ -16,25 +22,30 @@ namespace Config
             PrintConfig(configuration);
             ReadFile("Project_Config.txt", ref configuration);
             PrintConfig(configuration);
-
             RequestConfiguration(configuration);
         }
 
+        //Read configurations from file
         public static void ReadFile(string fileName, ref Configuration configuration)
         {
+            // Get all configurations from class
             var properties = configuration.GetType().GetProperties();
             var file = File.ReadAllLines(fileName);
 
             foreach (var line in file)
             {
+                // Find line with configuration name and value
                 if (Regex.IsMatch(line, @"^\w") && line.Contains(':'))
                 {
+                    // Remove unused characters
                     string parameterToParse = line.Remove(line.IndexOf('/'));
                     parameterToParse = parameterToParse.Replace("\t", "");
                     parameterToParse = parameterToParse.Replace(" ", "");
                     string[] values = parameterToParse.Split(":", 2);
+                    // Find configuration which will be changed
                     var propToChange = properties.Where(p => p.Name.ToLower() == values[0].ToLower()).FirstOrDefault();
-                    var valueToSet = Deserialize(values[1], propToChange.PropertyType);
+                    // Set required type of value in file
+                    var valueToSet = Deserialize(values[1], propToChange.PropertyType, propToChange.Name);
                     propToChange.SetValue(configuration, valueToSet);
                 }
             }
@@ -42,6 +53,7 @@ namespace Config
             Console.WriteLine("\nFile {0} loaded..", fileName);
         }
 
+        //Write all current configuration into console
         public static void PrintConfig(Configuration config)
         {
             var properties = config.GetType().GetProperties();
@@ -52,50 +64,74 @@ namespace Config
                 {
                     Console.WriteLine("{0} : {1}", prop.Name, prop.GetValue(config));
                 }
-
+                // Handle exceptions if configuration was not yet set up
                 catch (NullReferenceException)
-                {
-                    Console.WriteLine("{0} : Not configured", prop.Name);
-                }
-                catch(System.Reflection.TargetInvocationException)
+                { }
+                catch (System.Reflection.TargetInvocationException)
                 { }
             }
         }
 
+        //Request single configuration element
         public static void RequestConfiguration(Configuration configuration)
         {
             var properties = configuration.GetType().GetProperties();
             Console.WriteLine("\nInsert configuration id..");
+            //User input for configuration
             var requestedConfig = Console.ReadLine();
             var returnedConfig = properties.FirstOrDefault(p => p.Name == requestedConfig);
-            if(returnedConfig != null)
+            if (returnedConfig != null)
             {
-                Console.WriteLine("{0} : {1}", returnedConfig.Name, returnedConfig.GetValue(configuration));
+                try
+                {
+                    Console.WriteLine("{0} : {1}", returnedConfig.Name, returnedConfig.GetValue(configuration));
+                }
+                catch (System.Reflection.TargetInvocationException)
+                {
+                    Console.WriteLine("{0} type is incorrect.", returnedConfig.Name);
+                }  
             }
             else
             {
+                //Configuration was not found
                 Console.WriteLine("{0} : Error", requestedConfig);
             }
+            //Repeat if pressed R
             Console.WriteLine("\nPress R to repeat or any key to quit..");
-            if(Console.ReadKey().Key == ConsoleKey.R)
+            if (Console.ReadKey().Key == ConsoleKey.R)
             {
                 RequestConfiguration(configuration);
             }
 
         }
-
-        private static object Deserialize(string input, Type toType)
+        //Deserialize string data into configuration
+        private static object Deserialize(string input, Type toType, string configName)
         {
-            if (toType == typeof(int))
-                return int.Parse(input);
-            if (toType == typeof(InboudStrat))
-                return Enum.Parse(typeof(InboudStrat), input);
-            if (toType == typeof(PowerSupp))
-                return Enum.Parse(typeof(PowerSupp), input);
-            if (toType == typeof(TimeSpan))
-                return TimeSpan.Parse(input);
+            try
+            {
+                if (toType == typeof(int))
+                    return int.Parse(input);
+                if (toType == typeof(InboudStrat))
+                    return Enum.Parse(typeof(InboudStrat), input);
+                if (toType == typeof(PowerSupp))
+                    return Enum.Parse(typeof(PowerSupp), input);
+                if (toType == typeof(TimeSpan))
+                    return TimeSpan.Parse(input);
 
-            throw new NotImplementedException(toType.ToString());
+                throw new NotImplementedException(toType.ToString());
+            }
+            //Catch exceptions if input in configuration file is invalid.
+            catch (FormatException)
+            {
+                Console.WriteLine("Exception resolving {0}. Value {1} is not of type {2}.", configName, input, toType.ToString());
+                return null;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Exception resolving {0}. Value {1} is not of type {2}.", configName, input, toType.ToString());
+                return null;
+            }
+
         }
     }
 
@@ -108,6 +144,7 @@ namespace Config
         {
             get
             {
+                // throw exception if the value was not set
                 if (object.Equals(_ordersPerHour, default(int)))
                     throw new NullReferenceException();
                 return _ordersPerHour;
